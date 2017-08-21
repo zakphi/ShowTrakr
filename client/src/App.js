f3defc4a59f08d3c470e9f282364b5b42db58bbe
Philip Zak <zakphi@gmail.com>
2017-08-21 16:20:56 -0400

    remove Nav component

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,6 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home'
 import Login from './components/Login';
-import Nav from './components/Nav';
 import Register from './components/Register';
 
 class App extends Component {
