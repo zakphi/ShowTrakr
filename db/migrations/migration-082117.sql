ef2bc54ae5e79129143bfdafc0afc56d6cf6f2e5
Alexander Calleia <alex_calleia@yahoo.com>
2017-08-21 16:20:07 -0400

    add classname

--- db/migrations/migration-082117.sql
+++ db/migrations/migration-082117.sql
@@ -1,9 +1,9 @@
 \c tv_show_trackr_dev;
 
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL
+    last_name VARCHAR(255) NOT NULL,
     username VARCHAR(255) UNIQUE NOT NULL,
     password_digest TEXT NOT NULL,
     email VARCHAR(255) NOT NULL
@@ -12,10 +12,10 @@ CREATE TABLE IF NOT EXISTS user (
 CREATE TABLE IF NOT EXISTS shows (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255),
-    genre VARCHAR(255)
+    genre VARCHAR(255),
     sched_time VARCHAR(255),
     sched_day VARCHAR(255),
     image_url TEXT,
     summary TEXT,
-    user_id INT REFERENCES user(id)
-)
+    user_id INT REFERENCES users(id)
+);
