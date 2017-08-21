4f7aa47534a59f9215307b57be04fa99b2906cb3
Alexander Calleia <alex_calleia@yahoo.com>
2017-08-21 18:11:48 -0400

    add css tags

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Header = (props) => {
     return(
-        <header>
+        <header className="header">
             <h1>TV Show Trackr</h1>
             <nav className="navBar">
                 <li><Link to='/'>Home</Link></li>
