374e4afe222b07d89eceeb63cbd15f502fb57dc0
Dennis Luo <dennis.luo1@gmail.com>
2017-08-21 14:18:05 -0400

    set up basic routes

--- client/src/App.js
+++ client/src/App.js
@@ -12,13 +12,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header logOut={this.logOut} />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' render={() => <Login handleLoginSubmit={this.handleLoginSubmit} />} />
+        <Route exact path='/register' render={() => <Register handleRegisterSubmit={this.handleRegisterSubmit} />} /> 
+        <Footer />
       </div>
     );
   }
