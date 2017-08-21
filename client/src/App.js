540e0fcd21dfeae2d1e4d4637ed8d6871c5736d6
Philip Zak <zakphi@gmail.com>
2017-08-21 16:20:11 -0400

    import home component

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import {
 
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Home from './components/Home'
 import Login from './components/Login';
 import Nav from './components/Nav';
 import Register from './components/Register';
