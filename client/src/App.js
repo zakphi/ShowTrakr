27147f6af7654e2e20ee1819ff6c422dd6771a90
Dennis Luo <dennis.luo1@gmail.com>
2017-08-22 11:58:32 -0400

    set up single view 2/3

--- client/src/App.js
+++ client/src/App.js
@@ -27,6 +27,7 @@ class App extends Component {
     this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
     this.handleRegisterSubmit = this.handleRegisterSubmit.bind(this);
     this.logOut = this.logOut.bind(this);
+    this.getShowData = this.getShowData.bind(this);
   }
 
   // Auth Login/Register/Logout
@@ -82,13 +83,28 @@ class App extends Component {
       })
       .catch(err => console.log(err))
   }
+  
+  getShowData(showName) {
+    axios.get(`http://api.tvmaze.com/singlesearch/shows?q=${showName}`)
+    .then(res => {
+      console.log(res.data)
+      this.setState({
+        title: res.data.name,
+        genre: res.data.genre,
+        sched_time: res.data.sched_time,
+        sched_date: res.data.sched_date,
+        image_url: res.data.image_url,
+        summary: res.data.summary,
+      })
+    })
+  }
 
   render() {
     return (
       <Router>
         <div className="App">
           <Header logOut={this.logOut} auth={this.state.auth} />
-          <Route exact path='/' render={() => <Home dataLoaded={this.state.dataLoaded} popularShows={this.state.popularShows} />} />
+          <Route exact path='/' render={() => <Home dataLoaded={this.state.dataLoaded} popularShows={this.state.popularShows} getShowData={this.getShowData} />} />
           <Route exact path='/login' render={() => <Login handleLoginSubmit={this.handleLoginSubmit} />} />
           <Route exact path='/register' render={() => <Register handleRegisterSubmit={this.handleRegisterSubmit} />} />
           {this.state.redirect ? <Redirect push to={'/'} /> : ''}
