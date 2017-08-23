2018a4b233cb21322070984c67b3b975be7d1dac
Philip Zak <zakphi@gmail.com>
2017-08-23 12:12:28 -0400

    make route match new controller

--- routes/show-routes.js
+++ routes/show-routes.js
@@ -2,8 +2,7 @@ const express = require('express');
 const showRoutes = express.Router();
 const showsController = require('../controllers/shows-controller');
 
-
-showRoutes.get('/', showsController.userShowIndex);
+showRoutes.get('/', showsController.index);
 showRoutes.post('/', showsController.create);
 showRoutes.delete('/:id', showsController.delete);
 
