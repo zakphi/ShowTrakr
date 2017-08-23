2c4203c0e4834a9a1c5bf72c6ba709b345a8c448
Alexander Calleia <alex_calleia@yahoo.com>
2017-08-23 15:25:39 -0400

    add ternary for no image

--- client/src/components/Results.jsx
+++ client/src/components/Results.jsx
@@ -1,20 +1,13 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-
+import image from '../imgComingSoon.png'
 class Results extends Component {
-  checkEmpty(){
-    if(this.props.showResults.image === null){
-        return ''
-    } else {
-        return this.props.showResults.image.medium
-    }
-  }
   render(){
     return(
       <div className='show'>
         <Link to='/show'>
           <img onClick={() => this.props.getShowData(this.props.showResults.name)}
-            src={this.checkEmpty()}
+            src={this.props.showResults.image === null ? image : this.props.showResults.image.medium} 
             alt={this.props.showResults.name} /> 
           {this.props.showResults.name}
         </Link>
