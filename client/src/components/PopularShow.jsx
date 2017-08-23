8bd40ca2f5ea7d4d820bc88c157fdd24ad08415a
Alexander Calleia <alex_calleia@yahoo.com>
2017-08-23 15:24:51 -0400

    add ternary for no image and show page number

--- client/src/components/PopularShow.jsx
+++ client/src/components/PopularShow.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import image from '../imgComingSoon.png'
 const PopularShow = (props) => {
   return(
     <div className='show'>
       <Link to='/show'>
         <img onClick={() =>
           props.getShowData(props.show.name)}
-          src={props.show.image_thumbnail_path}
+          src={props.show.image_thumbnail_path === 'https://static.episodate.com' ? image : props.show.image_thumbnail_path}
           alt={props.show.name}
         />
       </Link>
