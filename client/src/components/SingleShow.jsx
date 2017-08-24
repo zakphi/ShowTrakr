a27518af1e2aa8972a91f3b74f32b0acbd39a448
adeseye sijuwade <adeseyesijuwade@gmail.com>
2017-08-24 11:38:15 -0400

    add favorite function to button

--- client/src/components/SingleShow.jsx
+++ client/src/components/SingleShow.jsx
@@ -2,21 +2,6 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class SingleShow extends Component {
-  addFavorite(){
-    axios.post('/profile', {
-      title: this.props.showData.title,
-      genre: this.props.showData.genre,
-      sched_time: this.props.showData.sched_time,
-      sched_day: this.props.showData.sched_day,
-      image_url: this.props.showData.image_url,
-      summary: this.props.showData.summary,
-    })
-    .then(res => {
-      console.log(res.data);
-    })
-    .catch(err => console.log(err));
-  }
-
   render(){
     return(
       <div className='singleShow'>
@@ -27,7 +12,7 @@ class SingleShow extends Component {
           <li>{this.props.showData.sched_time}</li>
         </ul>
         <p>{this.props.showData.summary}</p>
-        {this.props.auth ? <button className="favButton" onClick={() => this.addFavorite()}><Link to='/profile'>Favorite</Link></button> : ''}
+        {this.props.auth ? <button className="favButton" onClick={() => this.props.addFavorite()}><Link to='/profile'>Favorite</Link></button> : ''}
       </div>
     )
   }
