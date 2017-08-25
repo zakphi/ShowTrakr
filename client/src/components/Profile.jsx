87c24cb4a8a0cef6e77a70ed0fbfb519ddb53e47
adeseye sijuwade <adeseyesijuwade@gmail.com>
2017-08-25 10:12:40 -0400

    add functionality when viewing profile

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -2,19 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 
 const Profile = (props) => {
-  return (
-    <div className='show'>
-      <Link to='/show'>
-        {props.usersShows.map(show => {
-          return <img onClick={() =>
-            props.getShowData(show.title)}
-            key={show.id}
-            src={show.image_url}
-            alt={show.title} />
-        })}
-      </Link>
-    </div>
-  )
+  if(props.usersShows) {
+    return (
+      <div className='show'>
+        <Link to='/show'>
+          {props.usersShows.map(show => {
+            return <img onClick={() =>
+              props.getFavData(show)}
+              key={show.id}
+              src={show.image_url}
+              alt={show.title}
+              id={show.id}
+               />
+          })}
+        </Link>
+      </div>
+    )
+  } else {
+    return <h1>Loading...</h1>;
+  }
 }
 
 export default Profile;
