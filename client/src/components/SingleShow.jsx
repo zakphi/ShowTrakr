a3af0258901c420ef5be243ce8d00a6a49f39ca9
Alexander Calleia <alex_calleia@yahoo.com>
2017-08-26 17:14:23 -0400

    add container for fav buttons

--- client/src/components/SingleShow.jsx
+++ client/src/components/SingleShow.jsx
@@ -8,14 +8,16 @@ class SingleShow extends Component {
       <div className='singleShow'>
         <img src={this.props.showData.image_url} alt={this.props.showData.title} />
         <ul>
-          <li>{this.props.showData.title}</li>
+          <li><h1 className="title-of-show">{this.props.showData.title}</h1></li>
           <li>{this.props.showData.tv_network === '' ? this.props.showData.webChannel : this.props.showData.tv_network}</li>
           {this.props.showData.tv_network === '' ? '' : <li>{this.props.showData.sched_day}</li>}
           {this.props.showData.tv_network === '' ? '' : <li>{this.props.showData.sched_time}</li>}
         </ul>
         <p>{this.props.showData.summary}</p>
+        <div className="favBtn-container">
         {this.props.auth ? <button className="favButton" onClick={() => this.props.addFavorite()}><Link to='/profile'>Add</Link></button> : ''}
         {this.props.auth ? <button className="favButton" onClick={() => this.props.removeFavorite(this.props.imageClicked)}><Link to='/profile'>Remove</Link></button> : ''}
+        </div>
       </div>
     )
   }
