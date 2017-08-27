c9e3912e180f9cf1fe3001167caf1c169d1faf95
adeseye sijuwade <adeseyesijuwade@gmail.com>
2017-08-26 20:00:43 -0400

    change profile to include schedule

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -6,24 +6,52 @@ const Profile = (props) => {
     return (
       <div className="home">
         <h1 className="pageTitle">{localStorage.getItem('username')}'s Profile</h1>
-        <div className="popularShows">
-          <Link to='/show'>
+           <div className="allSched">
+            <h2 className="pageHeading">Your TV-Schedule</h2>
             {props.usersShows.map(show => {
-              return <div className='show'>
-                <img onClick={() =>
-                  props.getFavData(show)}
-                  key={show.id}
-                  src={show.image_url}
-                  alt={show.title}
-                  id={show.id}
-                />
-                <p>{show.title}</p>
-                <p>{show.genre}</p>
-                <p>{show.sched_day}</p>
-                <p>{show.sched_time}</p>
-              </div>
-            })}
-          </Link>
+              return (
+                <div className='schedule'>
+                  <div className='schedData'>
+                    <h3 className='schedHead'>Show</h3>
+                      <p>{show.title}</p>
+                  </div>
+                  <div className='schedData'>
+                    <h3 className='schedHead'>Airs on</h3>
+                     <p>{show.sched_day}</p>
+                  </div>
+                  <div className='schedData'>
+                    <h3 className='schedHead'>Airs at</h3>
+                     <p>{show.sched_time}</p>
+                  </div>
+                  <div className='schedData'>
+                    <h3 className='schedHead'>Network</h3>
+                     <p>{show.tv_network}</p>
+                  </div>
+                  <div className='schedData'>
+                    <h3 className='schedHead'>Web Channel</h3>
+                     <p>{show.web_channel}</p>
+                  </div>
+                </div>
+                )
+              })}
+          </div>
+          <div className="popularShows">
+           <h2 className="pageHeading">Favorites</h2>
+            {props.usersShows.map(show => {
+              return (
+                <div className='show'>
+                <Link to='/show'>
+                 <img onClick={() =>
+                   props.getFavData(show)}
+                   key={show.id}
+                   src={show.image_url}
+                   alt={show.title}
+                   id={show.id}
+                 />
+                </Link>
+                </div>
+                )
+              })}
         </div>
       </div>
     )
